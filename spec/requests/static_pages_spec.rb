331c5a2a18f5890ff24c284068dcaac069fde02a
Tranceable <midscript@gmail.com>
2016-01-19 20:45:54 +0400

    Some static, some test, add helper method

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,12 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
   end
 
@@ -24,20 +31,33 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
 
-  	it "should have the content 'About'" do
-  	  visit '/static_pages/about'
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
       expect(page).to have_content('About')
-  	end
+    end
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contacts" do
+
+  	it "should have the content 'Contacts'" do
+  	  visit '/static_pages/contacts'
+      expect(page).to have_content('Contacts')
+  	end
+
+    it "should have the title 'Contacts'" do
+      visit '/static_pages/contacts'
+      expect(page).to have_title("#{base_title} | Contacts")
     end
   end
 end
